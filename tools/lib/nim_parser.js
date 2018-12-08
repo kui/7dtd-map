7855ce8fe868beec850807f7d25298917bc4ff68
Keiichiro Ui <keiichiro.ui@gmail.com>
2018-12-08 13:57:23 +0900

    Improve nim file parse

--- tools/lib/nim_parser.js
+++ tools/lib/nim_parser.js
@@ -1,18 +1,21 @@
 const fs = require('fs');
 
-const nimInitOffset = 11;
-
 module.exports = async function parseNim(nimFileName) {
   const blocks = [];
   let buffer;
   let bufferIndex;
   let firstDigit = null;
-  let skipBytes = nimInitOffset;
+  let blockNum = null;
+  let prevDigits = [];
+  let skipBytes = 4;
   function handleByte(byte) {
     // console.log('16: %s, 10: %s, c: %s', Number(byte).toString(16),
     // byte, String.fromCharCode(byte));
     if (skipBytes !== 0) {
       skipBytes -= 1;
+    } else if (blockNum === null) {
+      blockNum = byte;
+      skipBytes = 6;
     } else if (buffer) {
       // console.log('pick as a char of block name: %s', String.fromCharCode(byte));
       // read block name chars
@@ -26,12 +29,19 @@ module.exports = async function parseNim(nimFileName) {
           blocks.push(blockName);
         }
         buffer = null;
-        skipBytes = 2;
+        if (blocks.length === blockNum) {
+          // console.log('parse all %d block', blockNum);
+          skipBytes = Infinity;
+        } else {
+          skipBytes = 2;
+        }
       }
     } else if (firstDigit === null) {
       // console.log('register first digit');
       firstDigit = byte;
+      prevDigits.unshift(byte);
     } else if (firstDigit === 0 && byte === 0) {
+      prevDigits.unshift(byte);
       // noop
     } else if (firstDigit === 0 && byte > 1) {
       // console.log('pick name length: %d', byte);
@@ -39,15 +49,28 @@ module.exports = async function parseNim(nimFileName) {
       buffer = Buffer.allocUnsafe(byte);
       bufferIndex = 0;
       firstDigit = null;
+      prevDigits = [];
     } else {
       firstDigit = null;
+      prevDigits.unshift(byte);
+    }
+    if (prevDigits.length !== 0 && prevDigits.length % 30 === 0) {
+      console.log(
+        'Unexpected state: Too long prevDigits: file=%s, length=%d',
+        nimFileName, prevDigits.length,
+      );
     }
   }
   const stream = fs.createReadStream(nimFileName);
   return new Promise((resolve, reject) => {
     stream.on('data', data => data.forEach(handleByte));
     stream.on('close', () => {
-      if (buffer) console.error('Unexpected state: %s', nimFileName);
+      if (buffer) {
+        console.error('Unexpected state: Unflushed buffer exists: %s', nimFileName);
+      }
+      if (blocks.length !== blockNum) {
+        console.error('Unexpected state: Unreach defined blocks num: %s', nimFileName);
+      }
       resolve(blocks);
     });
     stream.on('error', reject);
