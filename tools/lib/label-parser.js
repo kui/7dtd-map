cda1d7fa891a252fec31a40b942f995825e5fd8b
Keiichiro Ui <keiichiro.ui@gmail.com>
2020-06-30 23:23:33 +0900

    Fix missed label extractions

--- tools/lib/label-parser.js
+++ tools/lib/label-parser.js
@@ -3,10 +3,12 @@
 const fsPromise = require('fs').promises;
 const csvParse = require('csv-parse');
 
+const EN_INDEX = 5;
+
 module.exports = async (localizationFileName) => {
   const localization = await fsPromise.readFile(localizationFileName);
   const labelArr = await new Promise((resolve, reject) => {
     csvParse(localization, (err, out) => { resolve(out); reject(err); });
   });
-  return labelArr.reduce((r, l) => Object.assign(r, { [l[0]]: l[4] }), {});
+  return labelArr.reduce((r, l) => Object.assign(r, { [l[0]]: l[EN_INDEX] }), {});
 };
