71c7dab12d5d4cae3612208c415d62ce85f1e387
Keiichiro Ui <keiichiro.ui@gmail.com>
2021-06-22 00:47:52 +0900

    Setup to build typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,35 @@
 const path = require('path');
 const glob = require('glob');
+const { argv } = require('process');
 
 const entries = glob
-  .sync('./js/*.js')
-  .reduce((obj, current) => Object.assign(obj, { [path.basename(current)]: current }), {});
+  .sync('./src/*.ts')
+  .reduce((obj, current) => Object.assign(obj, { [path.basename(current, '.ts')]: current }), {});
+
+const isDev = argv.mode == 'development';
 
 module.exports = {
   entry: entries,
   output: {
-    filename: '[name]',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'docs'),
+  },
+  devtool: isDev ? 'inline-source-map' : 'source-map',
+  module: {
+    rules: [
+      { 
+        test: /\.ts$/, 
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+       },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  performance: {
+    hints: false,
   },
 };
