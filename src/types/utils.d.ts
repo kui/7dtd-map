18750df6e354ca33e5f47e01fcab2942906c07a4
Keiichiro Ui <keiichiro.ui@gmail.com>
2022-08-18 22:12:29 +0900

    Update dependencies

--- src/types/utils.d.ts
+++ src/types/utils.d.ts
@@ -1,13 +1,3 @@
-interface ObjectConstructor {
-  entries<T>(o: T): Entry<T>[];
-}
-
-type Entry<T> = NonNullable<
-  {
-    [K in keyof T]: [K, T[K]];
-  }[keyof T]
->;
-
 interface PngBlob extends Blob {
   type: "image/png";
 }
