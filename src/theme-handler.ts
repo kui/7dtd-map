5089e1e912d1fecd04e320dec90cf43af54e04f0
90sPixelDev <fontlien@gmail.com>
2023-01-02 14:30:52 -0500

    Starting the layout for the page

---
+++ src/theme-handler.ts
@@ -0,0 +1,20 @@
+const toggle = document.getElementById("theme-toggle");
+
+const storedTheme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+if (storedTheme) document.documentElement.setAttribute("data-theme", storedTheme);
+
+if (toggle) {
+  toggle.onclick = function () {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = "light";
+
+    if (currentTheme === "light") {
+      targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute("data-theme", targetTheme);
+    localStorage.setItem("theme", targetTheme);
+  };
+}
+console.log("Running theme Handler!");
