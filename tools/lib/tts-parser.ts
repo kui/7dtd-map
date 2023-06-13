d85e05d1d58a9ed3798d3222ebe88bae51b89d75
Keiichiro Ui <keiichiro.ui@gmail.com>
2023-06-13 21:21:41 +0900

    Block ID is 15 bits

--- tools/lib/tts-parser.ts
+++ tools/lib/tts-parser.ts
@@ -3,11 +3,8 @@ import { ByteReader } from "./byte-reader";
 import { BlockId } from "./nim-parser";
 
 // TTS format: https://7daystodie.gamepedia.com/Prefabs#TTS
-// There maight bee some defferences but I didn't know.
-// I think there are some changes around block data,
-// because block ID limit seems to increase to 32k from 2048
 const KNOWN_VERSIONS = [13, 15, 16];
-const BLOCK_ID_BIT_MASK = 0b0011111111111111;
+const BLOCK_ID_BIT_MASK = 0b0111111111111111;
 
 export async function parseTts(ttsFileName: string): Promise<Tts> {
   const stream = fs.createReadStream(ttsFileName);
