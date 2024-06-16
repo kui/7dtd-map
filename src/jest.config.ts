9f37dfbcd8c9c16d27a91f43989661c682395e2c
Keiichiro Ui <oshigoto@example.com>
2024-06-16 16:30:34 +0900

    Use esbuild for jest

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -1,4 +1,8 @@
-module.exports = {
-  preset: "ts-jest",
+import type { Config } from "jest";
+const config: Config = {
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.ts$": "esbuild-jest",
+  },
 };
+export default config;
