1c48e78c373fd4d3e235beda33453ce505829531
Keiichiro Ui <keiichiro.ui@gmail.com>
2024-07-24 20:32:46 +0900

    Format

--- tools/print-png-pixel-stat.ts
+++ tools/print-png-pixel-stat.ts
@@ -26,7 +26,9 @@ async function statPixels(pngFile: string): Promise<Map<string, number>> {
       .pipe(new PNG())
       .on("parsed", function () {
         for (let i = 0; i < this.data.length; i += 4) {
-          const pixels = Array.from(this.data.subarray(i, i + 4)).map(n => n.toString().padStart(3)).join(",");
+          const pixels = Array.from(this.data.subarray(i, i + 4))
+            .map((n) => n.toString().padStart(3))
+            .join(",");
           const pixelCount = pixelStat.get(pixels) ?? 0;
           pixelStat.set(pixels, pixelCount + 1);
         }
