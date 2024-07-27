867c153d4e732c2dec2c0fcca205b925c1c6eefd
Keiichiro Ui <keiichiro.ui@gmail.com>
2024-07-28 03:17:26 +0900

    Buttons to load bundled worlds instead of a single button for a sample world

--- src/index/file-handler.ts
+++ src/index/file-handler.ts
@@ -1,5 +1,4 @@
-import { throttledInvoker } from "../lib/throttled-invoker";
-import { waitAnimationFrame } from "../lib/utils";
+import { printError, waitAnimationFrame } from "../lib/utils";
 import { LoadingHandler } from "./loading-handler";
 
 interface Doms {
@@ -8,17 +7,17 @@ interface Doms {
 
 type Listener = (file: File) => unknown;
 
+type ResourceLike = File | { name: string; url: string };
+
 export class FileHandler {
-  private doms: Doms;
-  private listeners = new Map<RegExp | string, Listener[]>();
-  loadingHandler: LoadingHandler;
+  #listeners = new Map<RegExp | string, Listener[]>();
+  #loadingHandler: LoadingHandler;
 
   constructor(doms: Doms, loadingHandler: LoadingHandler) {
-    this.doms = doms;
-    this.loadingHandler = loadingHandler;
-
-    const throttledProcess: () => unknown = throttledInvoker(() => this.processFiles());
-    doms.input.addEventListener("input", throttledProcess);
+    this.#loadingHandler = loadingHandler;
+    doms.input.addEventListener("input", () => {
+      if (doms.input.files) this.pushFiles(Array.from(doms.input.files));
+    });
   }
 
   addListeners(arr: [RegExp | string, Listener | Listener[]][]): void {
@@ -26,68 +25,63 @@ export class FileHandler {
   }
 
   addListener(fileName: RegExp | string, listener: Listener | Listener[]): void {
-    const old = this.listeners.get(fileName) ?? [];
-    this.listeners.set(fileName, old.concat(listener));
+    const old = this.#listeners.get(fileName) ?? [];
+    this.#listeners.set(fileName, old.concat(listener));
   }
 
   pushFiles(files: File[]): void {
-    const filtered = files.filter((file) => this.getListeners(file.name).length !== 0);
-    this.updateFiles([...Array.from(this.doms.input.files ?? []), ...filtered]);
-    this.doms.input.dispatchEvent(new Event("input"));
+    this.#process(files).catch(printError);
   }
 
-  private async processFiles() {
-    this.loadingHandler.add(Array.from(this.doms.input.files ?? []).map((f) => f.name));
-    let file = this.popFile();
-    while (file instanceof File) {
-      const listeners = this.getListeners(file.name);
+  pushUrls(urls: string[]): void {
+    this.#process(urls.map((url) => ({ name: basename(url), url }))).catch(printError);
+  }
+
+  async #process(resourceList: ResourceLike[]) {
+    if (this.#loadingHandler.isLoading()) {
+      throw new Error("Loading is in progress");
+    }
+
+    this.#loadingHandler.add(resourceList.map((f) => f.name));
+    for (const resource of resourceList) {
+      const listeners = this.#getListeners(resource.name);
       if (listeners.length === 0) {
-        console.log("No hundler: ", file);
-      } else if (this.shouldSkip(file)) {
-        console.log("Skip: ", file);
+        console.log("No hundler: ", resource.name);
+      } else if (shouldSkip(resourceList, resource.name)) {
+        console.log("Skip: ", resource.name);
       } else {
-        console.time(`Processed: ${file.name}`);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        await Promise.all(listeners.map((fn) => fn(file!)));
-        console.timeEnd(`Processed: ${file.name}`);
+        console.time(`Processe: ${resource.name}`);
+        const f = await resolveResource(resource);
+        await Promise.all(listeners.map((fn) => fn(f)));
+        console.timeEnd(`Processe: ${resource.name}`);
       }
-      this.loadingHandler.delete(file.name);
+      this.#loadingHandler.delete(resource.name);
       await waitAnimationFrame();
-      file = this.popFile();
     }
   }
 
-  private getListeners(fileName: string) {
-    return Array.from(this.listeners.entries()).flatMap(([pattern, listeners]) => {
+  #getListeners(fileName: string) {
+    return Array.from(this.#listeners.entries()).flatMap(([pattern, listeners]) => {
       if (pattern instanceof RegExp && pattern.test(fileName)) return listeners;
       if (typeof pattern === "string" && pattern === fileName) return listeners;
       return [];
     });
   }
-
-  private popFile(): File | undefined {
-    if (!this.doms.input.files?.length) return undefined;
-    const files = Array.from(this.doms.input.files);
-    this.updateFiles(files.slice(1));
-    return files[0];
-  }
-
-  private shouldSkip(file: File): boolean {
-    if (file.name === "splat3.png") {
-      return Array.from(this.doms.input.files ?? [])
-        .map((f) => f.name)
-        .includes("splat3_processed.png");
-    }
-    return false;
-  }
-
-  private updateFiles(files: File[]) {
-    this.doms.input.files = dataTransfer(files).files;
-  }
 }
 
-function dataTransfer(files: File[]): DataTransfer {
-  const dt = new DataTransfer();
-  for (const f of files) dt.items.add(f);
-  return dt;
+async function resolveResource(resource: ResourceLike): Promise<File> {
+  if (resource instanceof File) return resource;
+  const blob = await fetch(resource.url).then((res) => res.blob());
+  return new File([blob], resource.name, { type: blob.type });
+}
+
+function basename(path: string) {
+  return path.substring(path.lastIndexOf("/") + 1);
+}
+
+function shouldSkip(resources: ResourceLike[], targetName: string): boolean {
+  if (targetName === "splat3.png") {
+    return resources.some(({ name }) => name === "splat3_processed.png");
+  }
+  return false;
 }
