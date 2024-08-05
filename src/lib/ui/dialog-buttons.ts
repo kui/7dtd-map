bf382d23c92cfa0f8b8506016d4faec09ec0cb41
Keiichiro Ui <keiichiro.ui@gmail.com>
2024-08-06 01:16:58 +0900

    Fix invalid nesting

--- src/lib/ui/dialog-buttons.ts
+++ src/lib/ui/dialog-buttons.ts
@@ -8,16 +8,15 @@ export function init() {
       if (!(dialog instanceof HTMLDialogElement)) throw Error(`Unexpected element: ${dialogId}`);
       dialog.showModal();
     });
-
-    for (const button of document.querySelectorAll<HTMLButtonElement>("button[data-close-dialog-for]")) {
-      button.addEventListener("click", () => {
-        const dialogId = button.dataset["closeDialogFor"];
-        if (dialogId == null) return;
-        const dialog = dialogId === "" ? button.closest("dialog") : document.getElementById(dialogId);
-        if (!dialog) throw Error(`Dialog not found: ${dialogId}`);
-        if (!(dialog instanceof HTMLDialogElement)) throw Error(`Unexpected element: ${dialogId}`);
-        dialog.close("");
-      });
-    }
+  }
+  for (const button of document.querySelectorAll<HTMLButtonElement>("button[data-close-dialog-for]")) {
+    button.addEventListener("click", () => {
+      const dialogId = button.dataset["closeDialogFor"];
+      if (dialogId == null) return;
+      const dialog = dialogId === "" ? button.closest("dialog") : document.getElementById(dialogId);
+      if (!dialog) throw Error(`Dialog not found: ${dialogId}`);
+      if (!(dialog instanceof HTMLDialogElement)) throw Error(`Unexpected element: ${dialogId}`);
+      dialog.close("");
+    });
   }
 }
