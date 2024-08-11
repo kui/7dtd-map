aa6eaecbc645b501655ddb181f1adc54cca27078
Keiichiro Ui <keiichiro.ui@gmail.com>
2024-08-11 14:59:23 +0900

    Prevent unnecessary drag and drop event handling

--- src/index/dnd-handler.ts
+++ src/index/dnd-handler.ts
@@ -16,24 +16,26 @@ export class DndHandler {
 
   constructor(dom: Doms, dialogHandler: DialogHandler) {
     dom.dragovered.addEventListener("dragenter", (event) => {
+      if (!event.dataTransfer?.types.includes("Files")) return;
       event.preventDefault();
       dialogHandler.state = "dragover";
       dialogHandler.open();
     });
     dom.dragovered.addEventListener("dragover", (event) => {
+      if (!event.dataTransfer?.types.includes("Files")) return;
       event.preventDefault();
-      if (!event.dataTransfer) return;
       event.dataTransfer.dropEffect = "copy";
     });
     document.body.addEventListener("dragleave", (event) => {
+      if (!event.dataTransfer?.types.includes("Files")) return;
       // clientX and clientY are 0 when the mouse cursor is out of the window frame.
       if (dom.dragovered === event.target || !(event.clientX === 0 && event.clientY === 0)) return;
       event.preventDefault();
       dialogHandler.close();
     });
     dom.dragovered.addEventListener("drop", (event) => {
-      event.preventDefault();
       if (!event.dataTransfer?.types.includes("Files")) return;
+      event.preventDefault();
       this.#listeners.dispatchNoAwait({
         drop: {
           files: Array.from(event.dataTransfer.items).flatMap((item) => item.webkitGetAsEntry() ?? []),
